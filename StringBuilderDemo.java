df491ac26dc1ec7a9e93e2aa6c5a00f636a6cbaf
Mahesh Venkat <mahevenkat0505@gmail.com>
2018-04-23 15:16:06 +1000

    Initial Commit

---
+++ StringBuilderDemo.java
@@ -0,0 +1,7 @@
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+        String s1 = "this index of example";
+
+        System.out.println(s1.indexOf("is",4));
+    }
+}
